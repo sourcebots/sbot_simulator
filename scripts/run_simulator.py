dd5c1d4d041e4f86c53fb356cf9a088c4197e6f0
WillB97 <will@rudi.org.uk>
2025-08-03 13:42:52 +0100

    Update runner scripts

--- scripts/run_simulator.py
+++ scripts/run_simulator.py
@@ -3,10 +3,13 @@
 A script to run the project in Webots.
 
 Largely just a shortcut to running the arena world in Webots.
-Only functional in releases.
 """
+# ruff: noqa: E501
+from __future__ import annotations
+
 import sys
 import traceback
+from os.path import expandvars
 from pathlib import Path
 from shutil import which
 from subprocess import Popen
@@ -14,53 +17,84 @@ from subprocess import Popen
 if sys.platform == "win32":
     from subprocess import CREATE_NEW_PROCESS_GROUP, DETACHED_PROCESS
 
-try:
-    if not (Path(__file__).parent / 'simulator/VERSION').exists():
-        print("This script is only functional in releases.")
-        raise RuntimeError
+if (Path(__file__).parent / 'simulator/VERSION').exists():
+    print("Running in release mode")
+    SIM_BASE = Path(__file__).parent.resolve()
+else:
+    print("Running in development mode")
+    # Assume the script is in the scripts directory
+    SIM_BASE = Path(__file__).parents[1].resolve()
 
-    world_file = Path(__file__).parent / "simulator/worlds/arena.wbt"
+POSSIBLE_WEBOTS_PATHS = [
+    ("darwin", "/Applications/Webots.app/Contents/MacOS/webots"),
+    ("win32", "C:\\Program Files\\Webots\\msys64\\mingw64\\bin\\webotsw.exe"),
+    ("win32", expandvars("%LOCALAPPDATA%\\Programs\\Webots\\msys64\\mingw64\\bin\\webotsw.exe")),
+    # Attempt to use the start menu shortcut
+    ("win32", expandvars("%ProgramData%\\Microsoft\\Windows\\Start Menu\\Programs\\Cyberbotics\\Webots.lnk")),
+    ("win32", expandvars("%APPDATA%\\Microsoft\\Windows\\Start Menu\\Programs\\Cyberbotics\\Webots.lnk")),
+    ("linux", "/usr/local/bin/webots"),
+    ("linux", "/usr/bin/webots"),
+]
 
+
+def get_webots_parameters() -> tuple[Path, Path]:
+    """
+    Get the paths to the Webots executable and the arena world file.
+
+    :return: The paths to the Webots executable and the arena world file
+    """
+    world_file = SIM_BASE / "simulator/worlds/arena.wbt"
+
+    if not world_file.exists():
+        raise RuntimeError("World file not found.")
+
+    if not (SIM_BASE / "venv").exists():
+        raise RuntimeError("Please run the setup.py script before running the simulator.")
+
+    # Check if Webots is in the PATH
     webots = which("webots")
 
     # Find the webots executable, if it is not in the PATH
     if webots is None:
-        if sys.platform == "darwin":
-            webots = "/Applications/Webots.app/Contents/MacOS/webots"
-        elif sys.platform == "win32":
-            webots = "C:\\Program Files\\Webots\\msys64\\mingw64\\bin\\webotsw.exe"
-        elif sys.platform.startswith("linux"):
-            possible_paths = ["/usr/local/bin/webots", "/usr/bin/webots"]
-            for path in possible_paths:
+        for system_filter, path in POSSIBLE_WEBOTS_PATHS:
+            if sys.platform.startswith(system_filter):
+                print(f"Checking {path}")
                 if Path(path).exists():
                     webots = path
                     break
-            else:
-                print("Webots executable not found.")
-                raise RuntimeError
+
+    if webots is None or not Path(webots).exists():
+        raise RuntimeError("Webots executable not found.")
+
+    return Path(webots), world_file
+
+
+def main() -> None:
+    """Run the project in Webots."""
+    try:
+        webots, world_file = get_webots_parameters()
+
+        # Run the world file in Webots,
+        # detaching the process so it does not close when this script does
+        if sys.platform == "win32":
+            Popen(
+                [str(webots), str(world_file)],
+                creationflags=DETACHED_PROCESS | CREATE_NEW_PROCESS_GROUP,
+                # shell=True is needed to run from shortcuts
+                shell=(webots.suffix == ".lnk"),
+            )
         else:
-            print("Unsupported platform.")
-            raise RuntimeError
+            Popen([str(webots), str(world_file)], start_new_session=True)
+    except RuntimeError as e:
+        print(f"An error occurred: {e}")
+        input("Press enter to continue...")
+        exit(1)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        print(traceback.format_exc())
+        input("Press enter to continue...")
+        exit(1)
 
-    if not Path(webots).exists():
-        print("Webots executable not found.")
-        raise RuntimeError
 
-    if not (Path(__file__).parent / "venv").exists():
-        print("Please run the setup.py script before running the simulator.")
-        raise RuntimeError
-
-    # Run the world file in Webots,
-    # detaching the process so it does not close when this script does
-    if sys.platform == "win32":
-        Popen([webots, world_file], creationflags=DETACHED_PROCESS | CREATE_NEW_PROCESS_GROUP)
-    else:
-        Popen([webots, world_file], start_new_session=True)
-except RuntimeError:
-    input("Press enter to continue...")
-    exit(1)
-except Exception as e:
-    print(f"An error occurred: {e}")
-    print(traceback.format_exc())
-    input("Press enter to continue...")
-    exit(1)
+if __name__ == "__main__":
+    main()
